6b269f8b0d57021815175525de4cea731e6d24f6
b-ellis <bradjellis18@gmail.com>
2016-07-28 08:53:13 -0700

    first commit

--- apps/app.js
+++ apps/app.js
@@ -3,17 +3,25 @@ $(function(){
 		event.preventDefault();
 		var searchTerm = $('#query').val();
 		getRequest(searchTerm);
-	})
+	});
 });
 
 function getRequest(searchTerm){
 	var params = {
 		part: 'snippet',
 		key: 'AIzaSyAzWbfHgmKTmYLdscc3FyhEDTYzHymkkJ0',
-		q: searchTerm
+		q: searchTerm,
+		maxResults: 10
 	}
 	url = "https://www.googleapis.com/youtube/v3/search";
-	$.getJSON(url, params, function(data){
-		console.log(data);
+	$.get(url, params, function(data, items){
+		for(i=0; i < items.length; i++){
+		var myData = data.items[i]
+		};
+		$.each(myData, function(){
+			
+		});
+		console.log(myData);
 	});
 }
+
