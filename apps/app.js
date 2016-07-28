0554bd5fe9b6e139542d2244c22681520071973f
b-ellis <bradjellis18@gmail.com>
2016-07-28 08:59:42 -0700

    changes

--- apps/app.js
+++ apps/app.js
@@ -15,11 +15,10 @@ function getRequest(searchTerm){
 	}
 	url = "https://www.googleapis.com/youtube/v3/search";
 	$.get(url, params, function(data, items){
-		for(i=0; i < items.length; i++){
-		var myData = data.items[i]
-		};
+		for(i=0; i < items.length; i++)
+		var myData = data.items[i];
 		$.each(myData, function(){
-			
+			console.log(myData);
 		});
 		console.log(myData);
 	});
